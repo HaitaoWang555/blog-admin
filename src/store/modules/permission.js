ab1c0c1a9dd3d0fcca9679cb682509eb3965424a
wanghaitao <15124505701@163.com>
2019-04-19 10:22:12 +0800

    fix: change generateRoutes

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,4 +1,4 @@
-import { constantRoutes } from '@/router'
+import { asyncRoutes, constantRoutes } from '@/router'
 
 /**
  * Use meta.role to determine if the current user has permission
@@ -49,7 +49,14 @@ const mutations = {
 const actions = {
   generateRoutes({ commit }, roles) {
     return new Promise(resolve => {
-
+      let accessedRoutes
+      if (roles.includes('admin')) {
+        accessedRoutes = asyncRoutes || []
+      } else {
+        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+      }
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
     })
   }
 }

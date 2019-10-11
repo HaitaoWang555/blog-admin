56367c30502e9f77ffe1de303b77a9138cabb13c
wanghaitao <15124505701@163.com>
2019-10-11 16:59:12 +0800

    feat: prod

--- vue.config.js
+++ vue.config.js
@@ -18,9 +18,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/public',
+  publicPath: '/',
   outputDir: 'dist',
-  assetsDir: 'static',
+  assetsDir: 'admin',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   css: {
